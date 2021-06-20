007e39d6e448aee071ba5f23e8880f6ca53a3c64
marcosATr <marcos.trannin@gmail.com>
2021-06-20 18:38:15 -0300

    [ADD] defer loading method

---
+++ static/assets/newJS.js
@@ -0,0 +1,9 @@
+function Character(name, atk, def, life) {
+    this.name = name;
+    this.atk = atk;
+    this.def = def;
+    this.life = life;
+  }
+
+  allChars = Array.from(document.querySelectorAll('.'))
+  
